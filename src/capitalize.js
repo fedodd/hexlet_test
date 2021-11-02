7289185148f803f84dd3f4165b5fe55353c551ec
Fedor Semenov <fs@moderntoken.com>
2021-11-02 15:21:48 +0300

    add capitalize func for test

---
+++ src/capitalize.js
@@ -0,0 +1,13 @@
+function capitalize(value) {
+
+  if (value === '') {
+    return ''
+  }
+
+  const valueArray = value.split('');
+  const [firstLetter, ...stringEnd] = valueArray;
+
+  return firstLetter.toUpperCase() + stringEnd.join('');
+}
+
+console.log('result', capitalize('hello'))
