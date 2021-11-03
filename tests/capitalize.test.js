1c7687e218d75899d9f0847a5483fde37fbbc6c4
Fedor Semenov <fs@moderntoken.com>
2021-11-03 20:04:31 +0300

    update test by using assert lib

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,8 @@
+import {strict as assert} from 'assert';
 import capitalize from '../src/capitalize.js';
 
-
-if (capitalize('hello') !== 'Hello') {
-  throw 'didnt uppercase first letter'
-}
-
-if (capitalize('') !== '') {
-  throw 'empty line must return empty line'
-}
-
-console.log('all tests are complete');
+//   throw 'didnt uppercase first letter'
+assert.equal(capitalize('hello'), 'Hello');
+//   throw 'empty line must return empty line'
+assert.equal(capitalize(''), '');
+console.log('all tests done');
